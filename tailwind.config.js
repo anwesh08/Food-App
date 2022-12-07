49d45b759e53fae6d26f1a441a7665ae3f5a087e
Anwesh Mishra <anweshm3@gmail.com>
2022-12-07 19:33:27 +0530

    Fonts & Color Added

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightOrange: '#FF7000',
+        darkBackground: '#1B2430',
+        textGray: '#536162',
+        offWhite: '#DDDDDD',
+        
+      },
+      fontFamily: {
+        'librebaskerville': ['Libre Baskerville', 'sans-serif'],
+        'vollkorn': ['Vollkorn', 'sans-serif'],
+      },
+    },
   },
   plugins: [],
 }
